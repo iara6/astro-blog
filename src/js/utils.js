d82845758e0d570c4542cc400a4f59721c6e2300
Irene <grim6ace@gmail.com>
2026-03-10 02:00:14 +0300

    ver 11

--- src/js/utils.js
+++ src/js/utils.js
@@ -29,5 +29,24 @@ export function formatBlogPosts(posts, {
 
     if (filterOutFuturePosts && new Date(date) > new Date()) return acc;
 
+    // add post to acc
+    acc.push(post);
+
+    return acc;
   }, [])
+
+  // sort by date or randomize
+  if (sortByDate) {
+    filteredPosts.sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date))
+  } else {
+    filteredPosts.sort(() => Math.random() - 0.5)
+  }
+
+  // limit if number is passed
+  if (typeof limit === "number") {
+    return filteredPosts.slice(0, limit);
+  }
+  
+  return filteredPosts;
+
 }
